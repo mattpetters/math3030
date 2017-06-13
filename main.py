bdb9ba10ed698d5dd3ea22fbd97109578c217eb1
Matt Petters <matt@petters.io>
2017-06-13 15:35:38 -0400

    Making todo comments

--- main.py
+++ main.py
@@ -1,3 +1,9 @@
+# TODO: The problem. A line l and a point P in R3 are given.
+# (1) Determine whether the point P lies on the line l.
+# (2) If P is not in the line l, find the plane that contains l and P.
+# (3) Find equation of the plane that passes through P perpendicular to l. (4) Find the line that passes through P and perpendicular to l.
+
+
 class Point:
     def __init__(xcomp, ycomp, zcomp):
         self.x = xcomp
