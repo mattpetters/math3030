6c6caf63e1bfac0f541da5d8d63d8d3e0046c33f
Matt Petters <matt@petters.io>
2017-06-13 15:31:32 -0400

    Creating Point class

--- main.py
+++ main.py
@@ -1,5 +1,12 @@
+class Point:
+    def __init__(xcomp, ycomp, zcomp):
+        self.x = xcomp
+        self.y = ycomp
+        self.z = zcomp
+
+
 def main():
-    print "Program running"
+    print("Program running")
 
 
 if __name__ == "__main__":
