d1bca112597a69681da3da5dd0dcc68eed2d6ef4
Matt Petters <matt@petters.io>
2017-06-15 16:35:49 -0400

    Finishing line impl

--- Line.java
+++ Line.java
@@ -2,5 +2,35 @@ import java.util.*;
 import java.io.*;
 
 public class Line {
-	
+    Point initialPoint;
+    Vector directionVector;
+
+    
+      public Line() {
+        x = 0;
+        y = 0;
+        z = 0;
+      }
+
+      public Line(Point initPt, Vector dirVec) {
+          initialPoint = initPt;
+          directionVector = dirVec;
+      }
+
+      public void setInitialPoint(Point point) {
+        initialPoint = point;
+      }
+
+      public void setDirectionVector(Vector vector) {
+        directionVector = vector;
+      }
+
+      public void getInitialPoint() {
+        return initialPoint;
+      }
+
+      public double getDirectionVector() {
+        return directionVector;
+      }
+
 }
