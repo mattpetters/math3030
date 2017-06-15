39cf0cb2e19fcea3fb5c00ff18374ba9404f283b
Jason Herrera <jasonrh4513@bellsouth.net>
2017-06-15 17:07:17 -0400

    plane changes

--- Plane.java
+++ Plane.java
@@ -2,7 +2,22 @@ import java.util.*;
 import java.io.*;
 
 public class Plane {
-		public Plane() {
-			
-		}
+	Point initialPoint;
+	Vector normalVector;
+
+	public Plane() {
+		initialPoint = new Point(0, 0, 0);
+		normalVector = new Vector(0, 0, 0);
+	}
+
+	public Plane(Point p, Vector v) {
+		initialPoint = p;
+		normalVector = v;
+	}
+
+	public void setInitialPoint(Point p) {
+		initialPoint = p;
+	}
+
+
 }
